93594a9fbb7effaf0ed846e2ea93bc498b7680ea
Md. Sadek Hossain Pranto <diu.shpranto.edu.bd@gmail.com>
2025-01-07 19:53:59 +0600

    Pattern 1

---
+++ Pattern 1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int row, col, n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    for(int row=1; row<=n; row++)
+    {
+        for(col=1; col<=row; col++)
+        {
+            printf("%d", col);
+        }
+        printf("\n");
+    }
+    return 0;
+}
